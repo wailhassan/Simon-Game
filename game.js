2c2de96b70ea168099a9ada709e1e196211e9fd6
wailhassan <wail.alammari@gmail.com>
2025-11-12 07:54:48 +0000

    Upload Simon Game fils

---
+++ game.js
@@ -0,0 +1,7 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+}
